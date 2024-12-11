4ed19ca3663ceef2022aba4cf33b8e617552cd79
rameshmmm <ramesh.manickam@cdw.com>
2024-12-11 16:49:20 +0530

    apollo server init

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
-//TODO
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema.js';
+
+(async function(){
+    const apolloServer = new ApolloServer({ typeDefs });      
+    const { url } = await startStandaloneServer(apolloServer);
+    console.log(`Our graphql server is ⬆️ and 🏃‍♂️ in ${url}`);
+})();
